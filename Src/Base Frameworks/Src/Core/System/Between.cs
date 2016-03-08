0171a09a9f1e85df71e4d05bb6d2fa71bd14fe96
Junil Um <powerumc@gmail.com>
2016-03-08 16:12:02 +0900

    add Between class.

---
+++ Src/Base Frameworks/Src/Core/System/Between.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	/// <summary>
+	/// 	구조체나 객체가 특정값 범위에 있는지 검사하는 클래스 입니다.
+	/// </summary>
+	/// <typeparam name="T"> Generic type parameter. </typeparam>
+	public class Between<T> where T : IComparable<T>, IEquatable<T>
+	{
+		/// <summary>
+		/// 	시작 값입니다.
+		/// </summary>
+		public T Begin { get; private set; }
+
+		/// <summary>
+		/// 	현재 값입니다.
+		/// </summary>
+		public T Now { get; private set; }
+
+		/// <summary>
+		/// 	마지막 값입니다.
+		/// </summary>
+		public T End { get; private set; }
+
+		/// <summary>
+		/// 	구조체나 객체가 특정값 범위에 있는지 검사하는 클래스의 생성자 입니다.
+		/// </summary>
+		/// <param name="begin"> 시작 값입니다. </param>
+		/// <param name="now">   현재 값입니다. </param>
+		/// <param name="end">   마지막 값입니다. </param>
+		public Between(T begin, T now, T end)
+		{
+			this.Begin = begin;
+			this.Now = now;
+			this.End = end;
+		}
+
+		/// <summary>
+		///		값이 사이에 있는지 여부를 검사합니다.
+		/// </summary>
+		public bool IsBetween
+		{
+			get { return this.Now.CompareTo(this.Begin) != -1 && this.Now.CompareTo(this.End) != 1; }
+		}
+
+		public bool IsOver
+		{
+			get { return this.Now.CompareTo(this.End) == 1; }
+		}
+}
+}
---
+++ Src/Base Frameworks/Tests/Core.Tests/System/Between_Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System
+{
+	[TestClass]
+	public class Between_Tests
+	{
+		[TestMethod]
+		public void Equals_Test()
+		{
+			var between = new Between<int>(1,1,1);
+			Console.WriteLine(between.IsBetween);
+
+			Assert.IsTrue(between.IsBetween);
+		}
+
+		[TestMethod]
+		public void IsBetween_Test()
+		{
+			var between = new Between<int>(1, 2, 3);
+			Console.WriteLine(between.IsBetween);
+
+			Assert.IsTrue(between.IsBetween);
+		}
+
+		[TestMethod]
+		public void IsNotBetween_Test()
+		{
+			var between = new Between<int>(1, 3, 2);
+			Console.WriteLine(between.IsBetween);
+
+			Assert.IsFalse(between.IsBetween);
+		}
+
+		[TestMethod]
+		public void IsOver_Test()
+		{
+			var between = new Between<int>(1, 3, 2);
+			Console.WriteLine(between.IsOver);
+
+			Assert.IsTrue(between.IsOver);
+		}
+
+		[TestMethod]
+		public void DateTimeBetween_Test()
+		{
+			var between = new Between<DateTime>(DateTime.Now.AddSeconds(-1), DateTime.Now, DateTime.Now.AddSeconds(1));
+			Console.WriteLine(between.IsBetween);
+
+			Assert.IsTrue(between.IsBetween);
+		}
+	}
+}
